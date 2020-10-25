ea93b382e38f7ac00a3e2f36d95a3f6e76d45879
Panduka De Silva <mspmax.pdn@gmail.com>
2020-10-26 10:37:55 +1100

    custom logger added

--- app/src/main/java/com/deltatre/samples/MainApplication.kt
+++ app/src/main/java/com/deltatre/samples/MainApplication.kt
@@ -1,7 +1,11 @@
 package com.deltatre.samples
 
 import android.app.Application
+import com.massive.mtclient.sdk.logs.MtLogger
 import com.mtribes.mtspace.Mtribes
+import timber.log.Timber
+import timber.log.Timber.DebugTree
+
 
 class MainApplication : Application() {
     override fun onCreate() {
@@ -9,7 +13,24 @@ class MainApplication : Application() {
             init(applicationContext)
             client.userTracking = true
             client.sessionLock = false // enable live updates
-            client.debugMode = true
+
+            if (BuildConfig.DEBUG) {
+                Timber.plant(DebugTree())
+
+                client.logger = object : MtLogger {
+                    override fun error(tag: String, message: String, throwable: Throwable?) {
+                        Timber.e(tag, message)
+                    }
+
+                    override fun info(tag: String, message: String) {
+                        Timber.i(tag, message)
+                    }
+
+                    override fun warn(tag: String, message: String) {
+                        Timber.w(tag, message)
+                    }
+                }
+            }
         }
         super.onCreate()
     }
